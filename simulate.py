be0df1f107d26d3fb78cde5ae6e1b0416e26df78
Tyler Hickey <dogbarnseries@gmail.com>
2023-01-30 21:37:16 -0500

    added floor

--- simulate.py
+++ simulate.py
@@ -1,9 +1,14 @@
+import pybullet_data
 import pybullet as p
 import time as time
 
 physicsClient = p.connect(p.GUI)
 
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 p.setGravity(0,0,-9.8)
+
+planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 
