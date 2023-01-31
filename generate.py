b976ba8b5658144dc772b4cb9ef9c59b99427155
Tyler Hickey <dogbarnseries@gmail.com>
2023-01-30 22:47:37 -0500

    added second link correctly

--- generate.py
+++ generate.py
@@ -16,9 +16,9 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     #pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1])
-    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5] , size=[length, width, height])
+    pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[length, width, height])
     pyrosim.End()
 
 Create_World()
