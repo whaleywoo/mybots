026f3f19234ffaf595a273e65cad4ab0fe48bbff
Tyler Hickey <dogbarnseries@gmail.com>
2023-01-30 21:58:56 -0500

    created box tower for loop

--- generate.py
+++ generate.py
@@ -9,6 +9,8 @@ y = 1
 z = 0.5
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[2,1,1.5] , size=[length, width, height])
+
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z + i] , size=[length, width, height])
+
 pyrosim.End()
