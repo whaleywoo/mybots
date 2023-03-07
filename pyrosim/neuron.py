197b5644cfef27a483f99ec6861487e42f0ca99a
Tyler Hickey <dogbarnseries@gmail.com>
2023-03-06 21:39:40 -0500

    fixed accidentally reverting scripts

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -71,12 +71,10 @@ class NEURON:
     
     def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0.0)
-        print(self.Get_Value())
         for synapse in synapses:
             if synapse[1] == self.Get_Name():
                 self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[synapse].Get_Weight(), neurons[synapse[0]].Get_Value())
-        print(self.Get_Value())
-        exit()
+        self.Threshold()
     
     def Allow_Presynaptic_Neuron_To_Influence_Me(self, synapse_weight, neuron_value):
         result = synapse_weight * neuron_value
